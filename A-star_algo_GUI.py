ec232616f3d25ee8176169cdc9549be9c18f2cf8
chenxk619 <68173712+chenxk619@users.noreply.github.com>
2020-11-17 22:07:24 +0800

    Update A-star_algo_GUI.py

--- A-star_algo_GUI.py
+++ A-star_algo_GUI.py
@@ -3,12 +3,19 @@ import sys
 from A_star_algo import *
 
 pygame.init()
-size = width, height = 400, 400
+size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
 
+class Board_gui:
+	def __init__(self):
+		self.x_lines = Board.x_length + 1
+		self.y_lines = Board.y_length + 1
 
+
+
+#Main game loop
 while True:
-	mouse_position = 'None'
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
@@ -19,5 +26,5 @@ while True:
 
 
 
-	screen.fill((0,0,0))
+	screen.fill((255, 255, 255))
 	pygame.display.update()
