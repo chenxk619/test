15bf64639cd8230a3322a040169c7a78b4b53435
chenxk619 <68173712+chenxk619@users.noreply.github.com>
2020-11-17 20:07:34 +0800

    Update A-star_algo_GUI.py

--- A-star_algo_GUI.py
+++ A-star_algo_GUI.py
@@ -8,9 +8,16 @@ screen = pygame.display.set_mode(size)
 
 
 while True:
+	mouse_position = 'None'
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
+
+		if pygame.mouse.get_pressed()[0]:
+			mouse_position = pygame.mouse.get_pos()
+			print(mouse_position)
+
+
 
 	screen.fill((0,0,0))
 	pygame.display.update()
