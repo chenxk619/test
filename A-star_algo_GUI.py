82ad007296d2e144a519bd5382bdf686ca5d8714
chenxk619 <68173712+chenxk619@users.noreply.github.com>
2020-11-18 20:35:43 +0800

    Update A-star_algo_GUI.py

--- A-star_algo_GUI.py
+++ A-star_algo_GUI.py
@@ -22,36 +22,44 @@ def board_init_state():
 	pygame.display.update()
 
 
-def game(start_pos, end_pos, start_state):
+def game(start_pos, end_pos):
+	barricades = []
+	game_start = False
+	start_state = False
 	#Main game loop
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
 
-			if event.type == pygame.K_SPACE:
+			if pygame.key.get_pressed()[pygame.K_SPACE]:
 				start_state = True
 
 			#Select start node pos
-			if pygame.mouse.get_pressed()[0] and event.type == pygame.K_1:
+			if pygame.mouse.get_pressed()[0] and pygame.key.get_pressed()[pygame.K_1]:
 				mouse_position = pygame.mouse.get_pos()
-				start_pos = [mouse_position[0] // 10, mouse_position[1] // 10]
+				if start_pos is None:
+					start_pos = [mouse_position[0] // 10, mouse_position[1] // 10]
 
 			#Select end node pos
-			if pygame.mouse.get_pressed()[0] and event.type == pygame.K_2:
+			if pygame.mouse.get_pressed()[0] and pygame.key.get_pressed()[pygame.K_2]:
 				mouse_position = pygame.mouse.get_pos()
-				end_pos = [mouse_position[0] // 10, mouse_position[1] // 10]
+				if end_pos is None:
+					end_pos = [mouse_position[0] // 10, mouse_position[1] // 10]
 
-			#Only able to select barricades if the 'game' hasnt started
-			if pygame.mouse.get_pressed()[0] and start_state == False and start_pos is not None and end_pos is not None:
+			#Only able to select barricades if the 'game' hasnt started and both start and end node pos are selected
+			if pygame.mouse.get_pressed()[0] and start_pos is not None and end_pos is not None and pygame.key.get_pressed()[pygame.K_3]:
 				mouse_position = pygame.mouse.get_pos()
-				print(mouse_position)
+				pos = [mouse_position[0] // 10, mouse_position[1] // 10]
+				if pos not in barricades:
+					barricades.append(pos)
+				print(barricades)
 
 			# Driver code
-			def main():
+			if start_state:
 				# start, end = start_end()
-				start, end = Nodes([1, 1], [6, 7], [1, 1], [0, 0], 0), Nodes([1, 1], [6, 7], [6, 7], [0, 0], 0)
-				visited, unvisited, barricades, node_path = set(), set(), [], []
+				start, end = Nodes(start_pos, end_pos, start_pos, [0, 0], 0), Nodes(start_pos, end_pos, end_pos, [0, 0], 0)
+				visited, unvisited, node_path = set(), set(), []
 				for i in range(30):
 					find(start, end, visited, unvisited, node_path, barricades)
 					print('=============')
@@ -63,7 +71,7 @@ def game(start_pos, end_pos, start_state):
 pygame.init()
 size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
-game(False, False, False)
+game(None, None)
 
 
 
