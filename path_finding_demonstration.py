348309ef31fdd70b42d9cf542927c4b87c7742bf
chenxk619 <68173712+chenxk619@users.noreply.github.com>
2020-12-12 15:48:17 +0800

    Create path_finding_demonstration.py

---
+++ path_finding_demonstration.py
@@ -0,0 +1,32 @@
+import pygame
+import sys
+
+
+
+
+def render():
+	pass
+
+def update():
+	pass
+
+def setup():
+	pass
+
+def game(screen, multiplayer):
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+
+
+def main():
+	multiplier = 15
+	pygame.init()
+	pygame.display.set_caption('A* pathing algo')
+	size = width, height = 50 * multiplier, 50 * multiplier
+	screen = pygame.display.set_mode(size)
+	game(screen, multiplier)
+	time.sleep(5)
+
+
+main()
